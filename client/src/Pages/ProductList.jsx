2bb65654b9efa729c4368de7de840ee4ef88be6f
Kumari Ranjana Yadav <yranjana757@gmail.com>
2024-02-21 20:24:23 +0530

    working on product pages

--- client/src/Pages/ProductList.jsx
+++ client/src/Pages/ProductList.jsx
@@ -1,18 +1,31 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import axios from "axios"
 
 const ProductList = () => {
+    const [data, setData] = useState([])
     const getAllData = async () => {
         const res = await axios.get("http://localhost:8000/get-products");
-        console.log(res?.data?.products
-        );
+        // console.log(res?.data?.products);
+        setData(res?.data?.products)
     }
 
     useEffect(() => {
         getAllData()
     }, [])
     return (
-        <div>ProductList</div>
+        <div className=''>
+            <div className='flex flex-wrap items-center'>
+                {
+                    data.map(item => (
+                        <div key={item.id} className='flex'>
+                            <img className='w-44' src={item.url} alt="" />
+                        </div>
+
+                    ))
+                }
+
+            </div>
+        </div>
     )
 }
 
