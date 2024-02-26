46586a2c9d7f2b846418b68efef428be233eb6e7
Kumari Ranjana Yadav <yranjana757@gmail.com>
2024-02-26 14:40:15 +0530

    user is successfully created while signup in backend

--- server/index.js
+++ server/index.js
@@ -4,13 +4,16 @@ const cors = require("cors");
 const  uploadData  = require("./uploadingDataToDB");
 const connectDB = require("./Database/dbConnect");
 const productRoute = require("./routes/route");
+const router = require("./routes/userRoute");
 
 
 
 const app = express();
 app.use(cors());
 app.use(express.urlencoded({extended:true}));
+app.use(express.json())
 app.use("/", productRoute)
+app.use("/" , router)
 
 const PORT = process.env.PORT || 8000;
 
