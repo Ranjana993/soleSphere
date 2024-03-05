9def7cf0f00198d8edf6e05c4a87d916daf90bd9
Kumari Ranjana Yadav <yranjana757@gmail.com>
2024-03-06 00:01:29 +0530

    working on collection page and crausel

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -33,7 +33,7 @@ const Header = () => {
                                 </svg>
                                 <span className="sr-only">Search icon</span>
                             </div>
-                            <input type="text" id="search-navbar" className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..." />
+                            <input type="text" className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..." />
                         </div>
                     </div>
                     <div className="md:order-2">
@@ -52,7 +52,7 @@ const Header = () => {
                                                 <div className=" absolute top-45 bg-red divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
                                                     <div className="px-4 py-3 text-sm text-gray-900 dark:text-white">
                                                         <div>Ranjana Yadav</div>
-                                                        
+
                                                     </div>
                                                     <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownUserAvatarButton">
                                                         <li>
