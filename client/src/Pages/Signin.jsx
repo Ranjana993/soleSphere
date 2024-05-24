1ba03d0a7cd0dac6f1a79716355b74b5eb7c5fc1
Kumari Ranjana Yadav <yranjana757@gmail.com>
2024-05-24 22:24:36 +0530

    added api call to signin

--- client/src/Pages/Signin.jsx
+++ client/src/Pages/Signin.jsx
@@ -1,6 +1,44 @@
-import { Link } from "react-router-dom"
+import axios from "axios";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { Link, useNavigate } from "react-router-dom"
 
 const Signin = () => {
+  const [userData, setUserData] = useState({ email: "", password: "" })
+
+  const navigate = useNavigate()
+
+  const onHandleChange = (e) => {
+    setUserData((prevUserData) => ({
+      ...prevUserData,
+      [e.target.name]: e.target.value
+    }));
+  };
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { email, password } = userData;
+    try {
+      const response = await axios.post('https://solesphere.onrender.com/signin', { email, password });
+      const data = response.data;
+      if (data.token) {
+        localStorage.setItem('token', data.token);
+        console.log('Sign-in successful');
+        toast.success('Successfully logged in user');
+        navigate('/');
+      } else {
+        console.log(data.msg);
+        toast.error('Sign-in failed: ' + data.msg);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      toast.error('Something went wrong: ' + error.message);
+    }
+  };
+
+
+
   return (
     <div className="">
       <section className="">
@@ -10,14 +48,14 @@ const Signin = () => {
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                 Sign in to your account
               </h1>
-              <form className="space-y-4 md:space-y-6">
+              <form onSubmit={handleSubmit} className="space-y-4 md:space-y-6">
                 <div>
                   <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                  <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required />
+                  <input type="email" name="email" value={userData.email} onChange={onHandleChange} id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required />
                 </div>
                 <div>
                   <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                  <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                  <input type="password" name="password" id="password" placeholder="••••••••" value={userData.password} onChange={onHandleChange} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
                 </div>
                 <div className="flex items-center justify-between">
                   <div className="flex items-start">
